546cd0b6d39662cb0e1cb81913130592812151b7
leticia-olive <leticiar@dbserver.com.br>
2024-04-12 10:14:41 -0300

    primeiros passos

---
+++ src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+
+    }
+}
